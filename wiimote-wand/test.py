8cab009483a47a0aba8421de6469e64be38d283b
Will Reynolds (pi) <wojreynolds2@gmail.com>
2020-01-09 21:32:09 +0000

    Successfully reading accelerometer data in g from wiimote

--- wiimote-wand/test.py
+++ wiimote-wand/test.py
@@ -19,13 +19,32 @@ while not wm:
 
 print("Successfully connected to Wiimote!")
 
-#wm.rpt_mode = cwwid.RPT_BIN | cwiid.RPT_ACC
+wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
+time.sleep(5)
 
-for i in range(16):
+wm.led = 1
+
+while True:
+    x = (wm.state['acc'][0]-120)/24.0
+    y = (wm.state['acc'][1]-120)/24.0
+    z = (wm.state['acc'][2]-120)/24.0
+    print('x: {0:.2f}'.format(x))
+    print('y: {0:.2f}'.format(y))
+    print('z: {0:.2f}'.format(z))
+    #wm.rumble = (wm.state['acc'][0] < 126)
+    if wm.state['buttons'] & cwiid.BTN_A:
+        wm.led = (wm.state['led'] + 1) % 16
+    time.sleep(.2)
+
+'''while(True):
+    print(wm.state)
+    time.sleep(1)'''
+
+'''for i in range(16):
     wm.led = i
     if i%3:
         wm.rumble = False
     else:
         wm.rumble = True
     time.sleep(0.5)
-wm.rumble = False
+wm.rumble = False'''
