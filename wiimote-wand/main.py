43ec406a2cc44ff68b0db0f83eee8565eddbe686
Will Reynolds (pi) <wojreynolds2@gmail.com>
2020-01-14 21:05:40 +0000

    Added simple state machine logic to prove puzzle mechanism

--- wiimote-wand/main.py
+++ wiimote-wand/main.py
@@ -9,16 +9,36 @@ from effects import Effect
 wiimote = Wiimote()
 effect = Effect()
 
+state = 0
+
 '''Main loop'''
 while True:
     acc = convertAcc(wiimote.accel(), 1)
     print('x: {0:.2f}'.format(acc[0]))
     print('y: {0:.2f}'.format(acc[1]))
     print('z: N/A')
-    wiimote.rumble(False)
-    if acc[1] > 45:
-       wiimote.rumble(True)
-       if not effect.busy():
-           effect.sound("audio/bubbles.wav")
+
+    if state == 0:
+        if acc[1] > 45:
+           wiimote.rumble(True)
+           if not effect.busy():
+               effect.sound("audio/bubble.wav")
+               state += 1
+
+    if state == 1:
+        if acc[0] > 45:
+            wiimote.rumble(True)
+            if not effect.busy():
+                effect.sound("audio/bubble.wav")
+                state += 1
+
+    if state == 2:
+        if acc[1] < -80:
+            wiimote.rumble(True)
+            if not effect.busy():
+                effect.sound("audio/bubbles.wav")
+                state = 0
+
     time.sleep(.2)
+    wiimote.rumble(False)
 
