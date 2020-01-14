a78cb98da05bffe5b7d420b0184b7b946d8bf70e
Will Reynolds (pi) <wojreynolds2@gmail.com>
2020-01-14 19:10:43 +0000

    Tidied up codebase

--- wiimote-wand/main.py
+++ wiimote-wand/main.py
@@ -2,21 +2,15 @@ import cwiid
 import time
 import pygame
 
-OFFSET = -120.0
-GAIN = 1.0/24.0
+from accel import convertAcc
 
-
-def convertAcc(raw):
-    return tuple([GAIN*(axis + OFFSET) for axis in raw])
-
-
-print("Press 1+2 on your Wiimote now...")
-
-wm = None
-i = 1
-pygame.mixer.init()
 
 '''Connect to Wiimote'''
+print("Press 1+2 on your Wiimote now...")
+
+i = 1
+wm = None
+
 while not wm:
     try:
         wm=cwiid.Wiimote()
@@ -35,17 +29,20 @@ wm.led = 9  # Show successful connection on Wiimote
 wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC
 time.sleep(1)  # Wait for configuration to be accepted and true values are returned
 
+'''Set up Audio'''
+pygame.mixer.init()
+
 '''Main loop'''
 while True:
-    acc = convertAcc(wm.state['acc'])
+    acc = convertAcc(wm.state['acc'], 1)
     print('x: {0:.2f}'.format(acc[0]))
     print('y: {0:.2f}'.format(acc[1]))
-    print('z: {0:.2f}'.format(acc[2]))
+    print('z: N/A')
     wm.rumble = False
-    if acc[1] > 0.2:
+    if acc[1] > 45:
         wm.rumble = True
         if pygame.mixer.music.get_busy() == False:
-            pygame.mixer.music.load("audio/clapping.wav")
+            pygame.mixer.music.load("audio/bubbles.wav")
             pygame.mixer.music.play()
     if wm.state['buttons'] & cwiid.BTN_A:
         wm.led = (wm.state['led'] + 1) % 16
