aab7a1d5143c321efec13918fad41cc8e8e83943
Will Reynolds (pi) <wojreynolds2@gmail.com>
2020-01-14 21:19:53 +0000

    Added flourishes as part of the spell process

--- wiimote-wand/main.py
+++ wiimote-wand/main.py
@@ -1,5 +1,6 @@
 import time
 
+from math import sqrt
 from accel import convertAcc
 
 from wii import Wiimote
@@ -13,27 +14,43 @@ state = 0
 
 '''Main loop'''
 while True:
-    acc = convertAcc(wiimote.accel(), 1)
-    print('x: {0:.2f}'.format(acc[0]))
-    print('y: {0:.2f}'.format(acc[1]))
-    print('z: N/A')
+    g = convertAcc(wiimote.accel(),0)
+    angles = convertAcc(wiimote.accel(), 1)
+    accMag = sqrt(g[0]**2 + g[1]**2 + g[2]**2)
+    print('x: {0:.2f}'.format(angles[0]))
+    print('y: {0:.2f}'.format(angles[1]))
+    print('Mag: {0:.2f}'.format(accMag))
 
     if state == 0:
-        if acc[1] > 45:
+        if angles[1] > 45:
            wiimote.rumble(True)
            if not effect.busy():
                effect.sound("audio/bubble.wav")
                state += 1
-
+    
     if state == 1:
-        if acc[0] > 45:
+        if accMag > 3:
+            wiimote.rumble(True)
+            if not effect.busy():
+                effect.sound("audio/bell2.wav")
+                state += 1
+
+    if state == 2:
+        if angles[0] > 45:
             wiimote.rumble(True)
             if not effect.busy():
                 effect.sound("audio/bubble.wav")
                 state += 1
+    
+    if state == 3:
+        if accMag > 3:
+            wiimote.rumble(True)
+            if not effect.busy():
+                effect.sound("audio/bell2.wav")
+                state += 1
 
-    if state == 2:
-        if acc[1] < -80:
+    if state == 4:
+        if angles[1] < -80:
             wiimote.rumble(True)
             if not effect.busy():
                 effect.sound("audio/bubbles.wav")
